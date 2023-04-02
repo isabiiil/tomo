ede828a3e19543011d1fe4807f2d1824afeb291f
Mac Koltovskiy <mackolt@DESKTOP-P2PHME8.localdomain>
2023-04-02 00:57:51 -0400

    Added timestamp functionality

--- src/data.js
+++ src/data.js
@@ -1,16 +1,16 @@
 
 function timeMinus(num) {
-    var date = new Date();
-    var day = date.getTime() - num;
-    date.setTime(day);
-    return date;
+    const date = Date.now();
+    const day = new Date(date);
+    day.setHours(day.getHours()-num)
+    return day;
 }
 export const data = [
-    {name:'sleep', details: '8 hours', timestamp: timeMinus(17.2 * 24 * 60 * 60 * 1000)},
-    {name:'factors', details: ['lorem','ipsum'], timestamp: timeMinus(15.28 * 24 * 60 * 60 * 1000)},
-    {name:'mood', details: 'lorem', timestamp: timeMinus(13.08 * 24 * 60 * 60 * 1000)},
-    {name:'energy levels', details: 'lorem', timestamp: timeMinus(11.64 * 24 * 60 * 60 * 1000)},
-    {name:'medications', details: ['lorem', 'ipsum', 'dolorum'], timestamp: timeMinus(8.40 * 24 * 60 * 60 * 1000)},
-    {name:'symptoms', details: 'lorem', timestamp: timeMinus(5.12 * 24 * 60 * 60 * 1000)},
+    {name:'sleep', details: '8 hours', timestamp: timeMinus(200)},
+    {name:'factors', details: ['lorem','ipsum'], timestamp: timeMinus(156.7 )},
+    {name:'mood', details: 'lorem', timestamp: timeMinus(130.87)},
+    {name:'energy levels', details: 'lorem', timestamp: timeMinus(116.4)},
+    {name:'medications', details: ['lorem', 'ipsum', 'dolorum'], timestamp: timeMinus(103)},
+    {name:'symptoms', details: 'lorem', timestamp: timeMinus(98)},
 
 ];
