5fe140a6869fd51674498a46d47c4ea73c996cc0
Ahmednyc1 <a.said030712@gmail.com>
2023-04-02 08:28:32 -0400

    sub

---
+++ src/connection.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import BottomNav from './BottomNav.jsx';
+import Ahmed from './ahmed.js';
+import Ahmed2 from './ahmed2.js';
+class MyComponent extends React.Component{
+render() {
+    return (
+      <div>
+        <BottomNav />
+      </div>
+    );
+  }
+}
