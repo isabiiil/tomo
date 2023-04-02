50efbaef4ab1bf058da775dc299f0ed5f41737e7
Spiros Goros <spiros@dynamic-oit-visitornet101-10-24-37-114.princeton.org>
2023-04-01 23:46:31 -0400

    fixed issues

--- src/TheEnd.js
+++ src/TheEnd.js
@@ -1,18 +1,27 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
 function PopUp() {
   const [message, setMessage] = useState("");
 
   useEffect(() => {
     const currentUrl = window.location.href;
+
+    const urls = [
+      "https://hack-princeton-s23.web.app/hydrate",
+      "https://hack-princeton-s23.web.app/social",
+    ];
+
+    // loop through the URLs and push them to the history state
+    urls.forEach((url) => {
+      window.history.pushState(null, null, url);
+    });
+
     if (currentUrl.indexOf("/hydrate") !== -1) {
       setMessage("Keep staying hydrated!");
-
     } else if (currentUrl.indexOf("/social") !== -1) {
       setMessage("Keep staying socially balanced!");
-
     } else {
-      console.log("[!} URL does not match a pop-up URL");
+      console.log("{!} URL does not match a pop-up URL");
     }
   }, []);
 
@@ -23,7 +32,7 @@ function PopUp() {
   );
 }
 
-function DisplayPopUp() {
+function App() {
   return (
     <div>
       <PopUp />
@@ -32,4 +41,5 @@ function DisplayPopUp() {
 }
 
 export default App;
+
 
